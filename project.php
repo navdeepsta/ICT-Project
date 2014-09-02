76382ac1bd06a160811270ee42623175e03c3488
niku1987 <navdeepst.1987@gmail.com>
2014-09-02 17:44:25 +1000

    Update project.php

--- project.php
+++ project.php
@@ -2,6 +2,7 @@
 <title></title>
 
 <body bgcolor = "gray">
+
 <form action="project.php" method="post">
 <input type="submit" name="university" value="University">
 </form>
@@ -11,7 +12,7 @@
  $con = pg_connect("host=ec2-54-204-35-114.compute-1.amazonaws.com   dbname=dbam19skhoh0fu     user=vuhbpfyiutxfgu    password=8zz2hhonBRPjl8wPPY2wyG-dQN")
  or die("Could not connect:".pg_last_error());
 
-$query  = " SELECT "Uni_id", "Name", location FROM "University" ";
+$query  = " SELECT \"Uni_id\", \"Name\", location FROM \"University\" ";
 
 $result = pg_query($con,$query) or die("Query failed:".pg_last_error());
 
